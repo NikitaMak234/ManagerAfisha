30508fc19b31d7c05d9ea13359b3faede8979619
NikitaMak234 <makarovnikita020395@yandex.ru>
2022-02-06 20:22:47 +0400

    Initial commit

---
+++ src/main/java/domain/Movie.java
@@ -0,0 +1,15 @@
+package domain;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Movie {
+    private int Id;
+    private String movieName;
+    private String posterUrl;
+    private String movieGenre;
+}
+
+
